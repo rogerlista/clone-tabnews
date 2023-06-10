c0c3fade80224b5db9755d5664407c37a7e39754
Rogério Gomes Rios <roger.lista@gmail.com>
2023-06-10 16:38:17 +0000

    chore: teste deploy vercel

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home() {
+    return <h1>Este espaço da internet esta sendo reservado, e espero que esse mesmo espaço ou até outros possa prosperar. Isso é para você 💞, se não entendeu não 👀</h1>
+}
+
+export default Home
